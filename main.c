32dac0c6c28b27c609f31576602a7b18ba8d7bb6
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-10-19 15:45:14 +0900

    실습3

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+int inc(int counter);
 int main(int argc, char *argv[]) {
-	int i;
-	
-	for(i=0; i<5;i++)
-	{
-		int temp=1;
-		printf("temp=%d\n",temp);
-		temp++;
-	}
+	int i=10;
+	printf("before i=%d\n",i);
+	inc(i);
+	printf("after i=%d\n",i);
 	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
